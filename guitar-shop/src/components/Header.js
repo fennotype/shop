dc9cc82a55b635bce55ab2e5eabb6c5e66756e3e
fennotype <bogdanovDV15@gmail.com>
2025-04-08 18:10:07 +0300

    добавлена библиотека express, файл server.js и подключение к db в файле db.js

--- guitar-shop/src/components/Header.js
+++ guitar-shop/src/components/Header.js
@@ -9,13 +9,18 @@ export default function Header() {
   return (
     <header>
         <div>
+        <FaShoppingCart onClick={()=> setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && `active`}`}/>
             <span className='logo'>Guitar Staff</span>
             <ul className='nav'>
               <li>про нас</li>
               <li>контакты</li>
               <li>кабинет</li>
             </ul>
-            <FaShoppingCart onClick={()=> setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && `active`}`}/>
+            
+
+              {cartOpen && { 
+             
+              }}
         </div>
         <div className='presentation'></div>
     </header>
